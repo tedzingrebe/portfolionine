4a195ce53c4068eb7cbb613ee26a1ef2af99dcfe
Ted <ted.zingrebe@gmail.com>
2014-09-02 18:04:04 -0500

    Revert ed4bec8..d834e7c
    This rolls back to commit ed4bec8d6eba729f0ccf4a80e287338eb62686d1.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,8 @@ Portfolio::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-
+  
+  
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
