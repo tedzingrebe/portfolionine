7074c2f7dba5b811b2ca448e94e76340021b2eae
Ted <ted.zingrebe@gmail.com>
2014-09-03 11:55:49 -0500

    added home page redirect

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Portfolio::Application.routes.draw do
   
   
   
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
