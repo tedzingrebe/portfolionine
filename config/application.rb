a694f4ee52c136ad62600b884d30f71614febb12
Ted <ted.zingrebe@gmail.com>
2014-09-09 21:02:34 -0500

    more mobile nav fun

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Portfolio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    #attempt mobile
+    config.serve_static_assets = true
+    
     # Required for Heroku
     config.assets.initialize_on_precompile = false
     
